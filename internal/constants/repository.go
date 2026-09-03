d960c4640084a3cfd9112dc6373f445f32e92973
RethikRaj <rethik.raj@ownly.food>
2026-09-03 13:53:11 +0530

    update db_collection_name

--- internal/constants/repository.go
+++ internal/constants/repository.go
@@ -1,6 +1,6 @@
 package constants
 
 const (
-	ETA_RESTAURANT_ESTIMATES string = "eta_restaurant_estimates"
-	ETA_SUBLOCALITY_ESTIMATES string = "eta_sublocality_estimates"
+	ETA_RESTAURANT_ESTIMATES string = "restaurant_components"
+	ETA_SUBLOCALITY_ESTIMATES string = "sublocality_components"
 )
