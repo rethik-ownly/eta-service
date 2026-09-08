1ed4687cf9b4b27ae18422bfc714f34755d521d5
RethikRaj <rethik.raj@ownly.food>
2026-09-08 20:07:05 +0530

    add orderId to kafka event

--- internal/types/eta_analytics_event.go
+++ internal/types/eta_analytics_event.go
@@ -5,6 +5,7 @@ import "github.com/nutanalabs/eta-service/internal/constants"
 type FetchEtaAnalyticsEvent struct {
 	EventId      string                 `json:"eventId"`
 	RequestId    string                 `json:"requestId"`
+	OrderId      string                 `json:"orderId,omitempty"`
 	CreatedAt    int64                  `json:"createdAt"`
 	EventType    constants.EventType    `json:"eventType"`
 	Surface      constants.Surface      `json:"surface"`
